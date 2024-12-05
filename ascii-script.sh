a2ccb1f37b911037152739da250cd5bc582f57f5
Ivan Lekarev <145346583+ilekarev@users.noreply.github.com>
2024-12-05 15:21:45 +0100

    added deps for jobs

--- ascii-script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Install cowsay
-sudo apt install cowsay -y
-# Generate ASCII Artwork
-cowsay -f dragon "Run for cover, I am a DRAGON....RAWR" >> dragon.txt
-# Read the file
-cat dragon.txt
